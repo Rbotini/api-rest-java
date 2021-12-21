2f43f5e2e75d2db9d8197203fe56807ec7b4349a
Renato Botini <botinirenato@gmail.com>
2021-12-21 15:05:05 -0300

    Adicionado o metodo Put para Alteração dos dados

--- api-rest/src/main/java/com/botidev/apirest/resources/ProdutoResource.java
+++ api-rest/src/main/java/com/botidev/apirest/resources/ProdutoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,31 +19,44 @@ import com.botidev.apirest.repository.ProdutoRepository;
 @RestController
 @RequestMapping(value = "/api")
 public class ProdutoResource {
-	
+
 	@Autowired
 	ProdutoRepository produtoRepository;
-	
-	// Lista Todos os Produtos
+
+	// Metodo Lista Todos os Produtos
 	@GetMapping("/produtos")
-	public List<Produto>listaProdutos(){
+	public List<Produto> listaProdutos() {
 		return produtoRepository.findAll();
 	}
-	
-	// listar Produtos Pelo ID
+
+	// Metodo listar Produtos passando Parametro id
 	@GetMapping("/produtos/{id}")
-	public  Produto  listaProdutosUnico(@PathVariable(value = "id")long id){
+	public Produto listaProdutosUnico(@PathVariable(value = "id") long id) {
 		return produtoRepository.findById(id);
 	}
-	
+
+	// metodo Post Para enviar os dados
 	@PostMapping("/produtos")
 	public Produto salvarProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
+
+	/*
+	 * Metodo Delete Passando Parametro id
+	 * 
+	 * @DeleteMapping("/produtos/{id}") public Produto
+	 * deleteUser(@PathVariable(value = "id")long id) { return
+	 * produtoRepository.deleteById(id); }
+	 */
 	
-	@DeleteMapping("/produtos/{id}")
-	public Produto deleteUser(@PathVariable(value = "id")long id) {
-		return produtoRepository.deleteById(id);
+	@DeleteMapping("/produtos")
+	public void deletarProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
 	}
 	
+	@PutMapping("/produtos")
+	public Produto AlterarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 
 }
