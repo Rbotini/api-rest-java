ab0967d845043cf7bce53cdc2ad8b806bb944d14
Renato Botini <botinirenato@gmail.com>
2021-12-21 12:16:24 -0300

    Criado Metodo Post e Testado

--- api-rest/src/main/java/com/botidev/apirest/resources/ProdutoResource.java
+++ api-rest/src/main/java/com/botidev/apirest/resources/ProdutoResource.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,12 @@ public class ProdutoResource {
 	public  Produto  listaProdutosUnico(@PathVariable(value = "id")long id){
 		return produtoRepository.findById(id);
 	}
+	
+	@PostMapping("/produtos")
+	public Produto salvarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	
 
 }
